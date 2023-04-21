ca509f83bd0817eb6248a96b7032a9d6c2643c7e
Nwanne Ezeaka <macnwanne@nwannes-MacBook-Pro.local>
2023-04-21 16:23:53 +0100

    update add loading spinner

--- app/(site)/loading.tsx
+++ app/(site)/loading.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function loading() {
   return (
       <div className='flex justify-center items-center h-screen'>
-          <div className='h-10 w-10 border-4 border-l-6 border-solid border-black border-l-orange-600 rounded-full animate-spin'>
+          <div className='h-10 w-10 border-4 border-l-6 border-solid border-transparent border-l-orange-600 rounded-full animate-spin'>
               
           </div>
     </div>
